56c7731d714f50f9c5fee30e25736c0eb078ae02
Bryan <bryan.gps18@gmail.com>
2024-11-08 13:52:31 -0300

    Service layer, component registration

--- src/main/java/com/zboxcross/ecommerce/resources/UserResource.java
+++ src/main/java/com/zboxcross/ecommerce/resources/UserResource.java
@@ -2,20 +2,33 @@ package com.zboxcross.ecommerce.resources;
 
 
 import com.zboxcross.ecommerce.entities.User;
+import com.zboxcross.ecommerce.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 
 @Controller
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
 
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(null,"Bryan");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
